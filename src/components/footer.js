6aa1acfe14b207afaa4235f087392e05f9c8fd63
abhinav <chetanya.a.nagineni@student.fh-kiel.de>
2017-12-22 13:07:38 +0100

    setting routes(home,header,footer)

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+
+const Footer = () => {
+    return(
+        <footer>
+            <div>Footer</div>
+        </footer>
+    
+    )
+}
+
+export default Footer;
